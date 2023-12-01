42868e3a37baa17108ffcf2724d114b48357fe11
신준혁 <oridori2705@naver.com>
2023-12-01 15:15:55 +0900

    commit: vercel 서버리스 사용을 위한 세팅 및 버전 충돌 오류 해결
    서버리스 함수에서 request와 resonse에 타입을 지정하는데 필요한 패키지를 import하면 버전 충돌 생김,
    vercel의 타입스크립트 버전과 프로젝트의 타입스크립트 버전의 충돌이 생김
    pakage.json에서 overrides로 해결

---
+++ api/hello.ts
@@ -0,0 +1,8 @@
+import { VercelRequest, VercelResponse } from '@vercel/node'
+
+export default function (req: VercelRequest, res: VercelResponse) {
+  res.status(200).json({
+    name: 'hi',
+    age: 12
+  })
+}
--- src/routes/MainPage.vue
+++ src/routes/MainPage.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+;(async () => {
+  const res = await fetch('/api/hello', {
+    method: 'POST'
+  })
+  const json = await res.json()
+  console.log(json)
+})()
+</script>
+
 <template>
   <h1>MainPage</h1>
 </template>
