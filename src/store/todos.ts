9c23d9e9482671b812e872d867e7d86b4db26679
신준혁 <oridori2705@naver.com>
2023-12-01 18:16:10 +0900

    commit: 할 일 목록 API 요청 및 store 저장
    타입 지정 유의

--- src/store/todos.ts
+++ src/store/todos.ts
@@ -1,12 +1,25 @@
 import { defineStore } from 'pinia'
 import axios from 'axios'
 
+export type Todos = Todo[] // 할 일 목록
+
+export interface Todo {
+  id: string // 할 일 ID
+  order: number // 할 일 순서
+  title: string // 할 일 제목
+  done: boolean // 할 일 완료 여부
+  createdAt: string // 할 일 생성일
+  updatedAt: string // 할 일 수정일
+}
+
 interface CreateTodoPayload {
   title: string
 }
 
 export const useTodosStore = defineStore('todos', {
-  state: () => ({}),
+  state: () => ({
+    todos: [] as Todos
+  }),
   getters: {},
   actions: {
     async createTodo({ title }: CreateTodoPayload) {
@@ -16,6 +29,12 @@ export const useTodosStore = defineStore('todos', {
           title
         }
       })
+    },
+    async fetchTodos() {
+      const { data } = await axios.post('/api/todos', {
+        method: 'GET'
+      })
+      this.todos = data
     }
   }
 })
